d76d42a6d87810da427f7d9f26a976d4f42e6b5c
Felix Maurer <felix@felix-maurer.de>
2018-11-28 11:39:23 +0100

    Plot the measured latencies
    As we are doing this with python, also add a file containig the
    dependencies.

---
+++ analysis/plot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+
+for filename in glob.iglob('../tslp/res-*.csv'):
+    basename = filename[8:-4]
+    data = pd.read_csv(filename, sep=';',
+                names=['timestamp', 'ip1', 'ip2', 'time1', 'time2', 'diff'],
+                index_col='timestamp')
+    data.index = pd.to_datetime(data.index, unit='s')
+
+    plt.figure()
+    ax = data.plot(linestyle='', marker='.', markersize=2,
+                y=['time1', 'time2'],
+                label=['near end', 'far end'],
+                color=['green', 'red'])
+    ax.set_xlabel('time')
+    ax.set_ylabel('latency (ms)')
+    plt.savefig(basename + '.pdf')
