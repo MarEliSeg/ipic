07b90d58219c3f9cbf4956bdc4147812cb536615
Felix Maurer <felix@felix-maurer.de>
2018-12-16 17:00:09 +0100

    Plot different routers in different colors

--- analysis/plot.py
+++ analysis/plot.py
@@ -2,7 +2,13 @@
 
 import glob
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+
+def get_colormap(ips, column):
+    colors = np.linspace(0, 1, len(ips))
+    colormap = dict(zip(ips, colors))
+    return [colormap[ip] for ip in column]
 
 for filename in glob.iglob('../tslp/res-*.csv'):
     basename = filename[8:-4]
@@ -11,13 +17,21 @@ for filename in glob.iglob('../tslp/res-*.csv'):
                 index_col='timestamp')
     data.index = pd.to_datetime(data.index, unit='s')
     data = data.loc[data.index > '2018-12-05T04:00:00']
+    ip2s = data['ip2'].unique()
 
-    plt.figure()
-    ax = data.plot(linestyle='', marker='.', markersize=2,
-                y=['time1', 'time2'],
-                ylim=(0, 80),
-                label=['near end', 'far end'],
-                color=['green', 'red'])
-    ax.set_xlabel('time')
-    ax.set_ylabel('latency (ms)')
+    fig = plt.figure()
+    plt.scatter(data.index, data['time1'],
+                s=1,
+                color='green')
+    plt.scatter(data.index, data['time2'],
+                s=1,
+                c=get_colormap(ip2s, data['ip2']),
+                cmap='autumn')
+    axes = plt.gca()
+    axes.set_ylim([0, 80])
+    axes.set_xlim(['2018-12-05', '2018-12-17'])
+    fig.autofmt_xdate()
+    plt.xlabel('time')
+    plt.ylabel('latency (ms)')
     plt.savefig(basename + '.pdf')
+    plt.close(fig)
